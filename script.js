891a1407f24d354a46fb626e22bed05a79dd1ea6
Laleh <laleh.keivani@gmail.com>
2026-04-06 20:39:22 -0400

    cleaned the commented part

--- script.js
+++ script.js
@@ -5,27 +5,6 @@ let timeout;
 
 let projects = [];
 
-// const projects = [
-//   {
-//     title: "Portfolio Website",
-//     description: "A responsive personal portfolio built using HTML, CSS, and JavaScript to showcase my projects.",
-//     category: "Web Design",
-//     img: "images/web-development.webp"
-//   },
-//   {
-//     title: "Task Manager App",
-//     description: "A simple task manager that allows users to add, delete, and manage daily tasks dynamically.",
-//     category: "JavaScript",
-//     img: "images/task-manager.png"
-//   },
-//   ,
-//   {
-//     title: "Weather App",
-//     description: "A web application that fetches real-time weather data using an external API.",
-//     category: "API Integration",
-//     img: "images/weather-app.jpg"
-//   }
-// ];
 
 async function loadProjects() {
     const response = await fetch("https://raw.githubusercontent.com/Lalehkei/web-foundations-UIComponents/feature/fetch-api/data/projects.json");
